aacd6d8c75fd32dcdae91875ffea0d93636ae698
kang0921ok <kang0921ok@naver.com>
2023-07-18 00:45:46 +0900

    [#27] age 기준 변경

--- lib/app/models/model_age_group.dart
+++ lib/app/models/model_age_group.dart
@@ -9,9 +9,9 @@ class ModelAgeGroup {
   static List<ModelAgeGroup> ageGroupList = [
     ModelAgeGroup(0, 0, 18, "0 ~ 18개월"), //겹치는 부분 동시노출
     ModelAgeGroup(1, 18, 3 * 12, "18 ~ 36개월"), //18개월~36개월(3살전) 겹치는 부분은 동시 노출
-    ModelAgeGroup(2, 3 * 12, 7 * 12, "유아"), //3살이상 ~ 7살 이전
-    ModelAgeGroup(3, 8 * 12, 10 * 12, "초등 저학년"), //초1(8),2(9),3(10)
-    ModelAgeGroup(4, 11 * 12, 13 * 12, "초등 고학년"), //초 4(11),5(12),6(13)
+    ModelAgeGroup(2, 3 * 12, 8 * 12, "유아"), //3살이상 ~ 초1
+    ModelAgeGroup(3, 8 * 12, 11 * 12, "초등 저학년"), //초1(8),2(9),3(10),4(11) 4학년 이하
+    ModelAgeGroup(4, 11 * 12, 14 * 12, "초등 고학년"), //초 4(11),5(12),6(13),중1(14) 중1 이하
   ];
 
   static ModelAgeGroup getAgeGroup(int groupId) {
